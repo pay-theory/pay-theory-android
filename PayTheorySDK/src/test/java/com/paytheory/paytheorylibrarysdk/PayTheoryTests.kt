caf11094291e7cf56c5c8e392279d7757baba1f0
Abel Collins <abel.dejesus.collins@gmail.com>
2020-11-18 13:36:53 -0500

    Update Testing

--- PayTheorySDK/src/test/java/com/paytheory/paytheorylibrarysdk/PayTheoryTests.kt
+++ PayTheorySDK/src/test/java/com/paytheory/paytheorylibrarysdk/PayTheoryTests.kt
@@ -2,8 +2,14 @@ package com.paytheory.paytheorylibrarysdk
 
 import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.paytheory.paytheorylibrarysdk.paytheory.BuyerOptions
 import com.paytheory.paytheorylibrarysdk.paytheory.CardPayment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.mockwebserver.MockResponse
@@ -11,8 +17,10 @@ import okhttp3.mockwebserver.MockWebServer
 import org.json.JSONObject
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 import org.robolectric.RobolectricTestRunner
 
 
@@ -21,6 +29,7 @@ import org.robolectric.RobolectricTestRunner
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(RobolectricTestRunner::class)
 class PayTheoryTests {
     private var client = OkHttpClient()
@@ -38,10 +47,11 @@ class PayTheoryTests {
         "phoneNumber",
         "email"
     )
-    var payTheory = PayTheory(context, "pt-sandbox-dev-d9de9154964990737db2f80499029dd6", cardPayment, buyerOptions)
+    private var payTheory = PayTheory(context, "pt-sandbox-dev-d9de9154964990737db2f80499029dd6", cardPayment, buyerOptions)
+    private var failedPayTheory = PayTheory(context, "wrong api", cardPayment, buyerOptions)
 
     @Test
-    fun testChallengeCall() {
+    fun test1ChallengeCall() {
         val request = Request.Builder()
             .url("https://dev.tags.api.paytheorystudy.com/challenge")
             .header("X-API-Key", payTheory.apiKey)
@@ -49,11 +59,68 @@ class PayTheoryTests {
 
         val response = client.newCall(request).execute()
         val jsonData: String? = response.body?.string()
+        val challengeJSONResponse = JSONObject(jsonData)
+        assertNotNull(challengeJSONResponse)
+        if (jsonData != null) {
+            assert(jsonData.contains("challenge"))
+        }
+    }
+
+    @Test
+    fun test2FailedChallengeCall() {
+        val request = Request.Builder()
+            .url("https://dev.tags.api.paytheorystudy.com/challenge")
+            .header("X-API-Key", failedPayTheory.apiKey)
+            .build()
+
+        val response = client.newCall(request).execute()
+        val jsonData: String? = response.body?.string()
         Log.e("PTLib", "Challenge response body $jsonData")
         val challengeJSONResponse = JSONObject(jsonData)
-
+        if (jsonData != null) {
+            assert(jsonData.contains("message"))
+        }
         assertNotNull(challengeJSONResponse)
+        assert(challengeJSONResponse.getString("message") == "Forbidden")
     }
+
+    //TODO - set up unit tests with coroutines
+//    @Test
+//    fun test3PayTheoryIdempotency() {
+//        var payTheoryIdempotency: String
+//        var nonce = payTheory.challenge()
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val attestationResponse = async {
+//                payTheory.attestation(nonce)
+//            }.await()
+//            payTheoryIdempotency = payTheory.payTheoryIdempotency(nonce, attestationResponse)
+//        }
+//        assert(payTheoryIdempotency.contains("response"))
+//        assert(payTheoryIdempotency.contains("signature"))
+//        assert(payTheoryIdempotency.contains("credId"))
+//
+//        assert(payTheoryIdempotency.isNotBlank())
+//    }
+//
+//    @Test
+//    fun test3FailedPayTheoryIdempotency() {
+//        var nonce = payTheory.challenge()
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val attestationResponse = async {
+//                payTheory.attestation(nonce)
+//            }.await()
+//            var payTheoryIdempotency = payTheory.payTheoryIdempotency(nonce, attestationResponse)
+//
+//
+//            assert(payTheoryIdempotency.contains("response"))
+//            assert(payTheoryIdempotency.contains("signature"))
+//            assert(payTheoryIdempotency.contains("credId"))
+//
+//            assert(payTheoryIdempotency.isNotBlank())
+//        }
+//    }
+
+
 
 
 }
