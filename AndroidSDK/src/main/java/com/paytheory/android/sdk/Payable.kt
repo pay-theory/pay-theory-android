e0483093f8d7fe8141df344a3701a118d9a4f891
Abel Collins <abel.dejesus.collins@gmail.com>
2021-03-15 10:52:06 -0400

    Documentation

--- AndroidSDK/src/main/java/com/paytheory/android/sdk/Payable.kt
+++ AndroidSDK/src/main/java/com/paytheory/android/sdk/Payable.kt
@@ -2,6 +2,19 @@ package com.paytheory.android.sdk
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Data class that represents the payment results after transaction has been processed
+ * @param receipt_number receipt/confirmation number for the transaction
+ * @param last_four last four of account number
+ * @param brand brand of card
+ * @param state current state of transaction
+ * @param amount amount of the transaction
+ * @param service_fee service fee amount
+ * @param tags custom tags that can be added to transaction
+ * @param created_at creation time
+ * @param updated_at updated time
+ * @param type ACH or CARD
+ */
 data class PaymentResult (
     @SerializedName("receipt_number") val receipt_number: String,
     @SerializedName("last_four") val last_four: String,
@@ -14,12 +27,30 @@ data class PaymentResult (
     @SerializedName("updated_at") val updated_at: String?,
     @SerializedName("type") val type: String?,)
 
+/**
+ * Data class that represents the error received if payment fails
+ * @param reason reason the transaction failed
+ */
 data class PaymentError (
     @SerializedName("reason") val reason: String,)
 
-
+/**
+ * Interface that handles a transaction completion, failure, and errors
+ */
 interface Payable {
+    /**
+     * Converts paymentResult as Payable
+     * @param paymentResult reason the transaction failed
+     */
     fun paymentComplete(paymentResult: PaymentResult)
+    /**
+     * Converts paymentFailure as Payable
+     * @param paymentFailure reason the transaction failed
+     */
     fun paymentFailed(paymentFailure: PaymentResult)
+    /**
+     * Converts paymentError as Payable
+     * @param paymentError reason the transaction failed
+     */
     fun paymentError(paymentError: PaymentError)
 }
