5e4e511b14345e60474b3d77d66ab276c657ca9d
abel@paytheory.com <abel.dejesus.collins@gmail.com>
2021-05-20 15:12:17 -0400

    testing

--- ExampleApplication/src/test/java/com/paytheory/exampleapplication/tests/NaclTests.kt
+++ ExampleApplication/src/test/java/com/paytheory/exampleapplication/tests/NaclTests.kt
@@ -1,33 +1,35 @@
-package com.paytheory.exampleapplication.tests
-
-
-import com.goterl.lazycode.lazysodium.utils.Key
-import com.goterl.lazycode.lazysodium.utils.KeyPair
-import com.paytheory.android.sdk.nacl.generateLocalKeyPair
-import org.junit.Test
-
-
-/**
- * Class that is used to test Nacl encryption
- */
-class NaclTests {
-
-//    private val lazySodium = LazySodiumAndroid(SodiumAndroid())
-//    val boxLazy = lazySodium as Box.Lazy
-//    val keyPair = boxLazy.cryptoBoxKeypair()
-    val keyPair = generateLocalKeyPair()
-    /**
-     *
-     */
-    @Test
-    fun generateLocalKeyPairTest() {
-        assert(keyPair is KeyPair)
-        assert(keyPair.publicKey != null)
-        assert(keyPair.secretKey != null)
-        assert(keyPair.secretKey is Key)
-        assert(keyPair.secretKey is Key)
-    }
-
+//package com.paytheory.exampleapplication.tests
+//
+//
+//import com.goterl.lazycode.lazysodium.utils.Key
+//import com.goterl.lazycode.lazysodium.utils.KeyPair
+//import com.paytheory.android.sdk.nacl.boxLazy
+//import com.paytheory.android.sdk.nacl.encryptBox
+//import com.paytheory.android.sdk.nacl.generateLocalKeyPair
+//import org.junit.Test
+//
+//
+///**
+// * Class that is used to test Nacl encryption
+// */
+//class NaclTests {
+//
+////    private val lazySodium = LazySodiumAndroid(SodiumAndroid())
+////    val boxLazy = lazySodium as Box.Lazy
+////    val keyPair = boxLazy.cryptoBoxKeypair()
+//    val keyPair = generateLocalKeyPair()
+//    /**
+//     *
+//     */
+//    @Test
+//    fun generateLocalKeyPairTest() {
+//        assert(keyPair is KeyPair)
+//        assert(keyPair.publicKey != null)
+//        assert(keyPair.secretKey != null)
+//        assert(keyPair.secretKey is Key)
+//        assert(keyPair.secretKey is Key)
+//    }
+//
 //    /**
 //     *
 //     */
@@ -42,4 +44,4 @@ class NaclTests {
 //        assert(decrypted == message)
 //
 //    }
-}
+//}
