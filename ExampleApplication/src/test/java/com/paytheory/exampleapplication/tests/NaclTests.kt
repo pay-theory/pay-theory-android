697508dfdae87e7764e0cef30561957111eaa2af
abel@paytheory.com <abel.dejesus.collins@gmail.com>
2021-05-20 15:07:41 -0400

    testing

---
+++ ExampleApplication/src/test/java/com/paytheory/exampleapplication/tests/NaclTests.kt
@@ -0,0 +1,45 @@
+package com.paytheory.exampleapplication.tests
+
+
+import com.goterl.lazycode.lazysodium.utils.Key
+import com.goterl.lazycode.lazysodium.utils.KeyPair
+import com.paytheory.android.sdk.nacl.generateLocalKeyPair
+import org.junit.Test
+
+
+/**
+ * Class that is used to test Nacl encryption
+ */
+class NaclTests {
+
+//    private val lazySodium = LazySodiumAndroid(SodiumAndroid())
+//    val boxLazy = lazySodium as Box.Lazy
+//    val keyPair = boxLazy.cryptoBoxKeypair()
+    val keyPair = generateLocalKeyPair()
+    /**
+     *
+     */
+    @Test
+    fun generateLocalKeyPairTest() {
+        assert(keyPair is KeyPair)
+        assert(keyPair.publicKey != null)
+        assert(keyPair.secretKey != null)
+        assert(keyPair.secretKey is Key)
+        assert(keyPair.secretKey is Key)
+    }
+
+//    /**
+//     *
+//     */
+//    @Test
+//    fun encryptBoxTest() {
+//        val message = "Secret Message"
+//
+//        val cipher = encryptBox(message, keyPair.publicKey)
+//
+//        val decrypted: String = boxLazy.cryptoBoxSealOpenEasy(cipher, keyPair)
+//
+//        assert(decrypted == message)
+//
+//    }
+}
