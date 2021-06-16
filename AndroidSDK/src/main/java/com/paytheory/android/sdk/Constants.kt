e7e167beaee159175f51fb627432829881f11a1e
abel@paytheory.com <abel.dejesus.collins@gmail.com>
2021-06-16 11:43:59 -0400

    Update token api call

--- AndroidSDK/src/main/java/com/paytheory/android/sdk/Constants.kt
+++ AndroidSDK/src/main/java/com/paytheory/android/sdk/Constants.kt
@@ -3,12 +3,12 @@ package com.paytheory.android.sdk
 /**
  * Object that contains constant variables
  */
-class Constants(env: String, stage: String) {
+class Constants(partner: String, stage: String) {
     val API_BASE_PATH: String
     val NO_INTERNET_ERROR: String
 
     init {
-         API_BASE_PATH = "https://$env.token.service.$stage.com/"
+         API_BASE_PATH = "https://$partner.token.service.$stage.com/$partner/"
          NO_INTERNET_ERROR = "No internet connection"
     }
 }
